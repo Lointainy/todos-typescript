836df66c7bea643bf4508666aeb43fa7e43659be
Eugene_dev <lointainyyy@gmail.com>
2022-11-05 11:17:43 +0200

    fix

--- src/theme.ts
+++ src/theme.ts
@@ -1,10 +1,10 @@
-let theme = localStorage.getItem('user-theme') === null ? 'light-theme' : getTheme()
+let theme: string = localStorage.getItem('user-theme') === null ? 'light-theme' : getTheme()
 
 function getTheme() {
 	return JSON.parse(localStorage.getItem('user-theme') || '')
 }
 
-export const setTheme = async () => {
+export const setTheme = () => {
 	localStorage.getItem('user-theme') === null ? theme == 'light-theme' : theme == localStorage.getItem('user-theme')
 	localStorage.setItem('user-theme', JSON.stringify(theme))
 	document.documentElement.className = theme
