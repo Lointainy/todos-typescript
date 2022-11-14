ed0e52f5e166de4f21153dca3988e0b5370ba41b
Eugene_dev <lointainyyy@gmail.com>
2022-11-14 10:27:31 +0200

    added logic

--- src/main.ts
+++ src/main.ts
@@ -8,15 +8,21 @@ const todoName = document.querySelector<HTMLInputElement>('.todo__name')
 const todoList = document.querySelector<HTMLUListElement>('.todos-list')
 const todoCategory = document.querySelector<HTMLSelectElement>('.todo__category')
 const toggleThemeButton = document.querySelector<HTMLButtonElement>('.header-menu__theme')
+const removeCompletedTodosButton = document.querySelector<HTMLButtonElement>('.todos__remove')
+const defaultFilter = document.getElementById('all-todos') as HTMLInputElement | null
+const todosFilter: any = document.querySelectorAll('.todos__filter-status')
 
 /* Store */
 let todos: todoType[] = []
+let filteredTodos: todoType[] = []
 let category: string[] = ['work', 'home', 'other']
 
 /* Watch */
 todoCategory?.addEventListener('change', selectedCategory)
 addButton?.addEventListener('click', handleAddTodo)
 todoList?.addEventListener('click', handleChangeTodo)
+todosFilter?.forEach((input: any) => input.addEventListener('change', handlefilteredTodos))
+removeCompletedTodosButton?.addEventListener('click', handleRemoveCompletedTodos)
 
 toggleThemeButton?.addEventListener('click', toggleTheme)
 document.addEventListener('DOMContentLoaded', onMounted)
@@ -38,6 +44,16 @@ const getLocalTodos = () => {
 	return JSON.parse(localStorage.getItem('todos') || '')
 } // get TODOS from localStorage
 
+const changeTotalTodos = () => {
+	let totalTodosField = document.querySelector('.todos__total')
+
+	let total = todos.filter((i) => i.status == false).length
+
+	totalTodosField!.innerHTML = `
+	  ${total} items left
+	`
+}
+
 /* ADD TODO */
 function handleAddTodo() {
 	todoName!.value.length < 1 ? (todoName!.placeholder = 'small title') : (todoName!.placeholder = 'add new todos')
@@ -45,20 +61,18 @@ function handleAddTodo() {
 	if (todoName!.value.length >= 1) {
 		let todoId = getRandomId()
 
-		todoList!.innerHTML += `
-		<li id="${todoId}" class="todos-list__item">
-			<input type="checkbox" class="item__status">
-			<input type="text" value="${todoName!.value}" readonly class="item__title"/>
-			<span>${selectedCategory()}</span>
-			<button type="button" class="item__btn-edit btn">edit</button>
-			<button type="button" class="item__btn-remove btn">remove</button>
-		</li>`
-
 		todos = [...todos, { title: todoName!.value, id: todoId, status: false, category: selectedCategory() }] // push new TODO
 		setLocalTodos()
 
+		filteredTodos = todos
+
+		getTodos()
+
 		todoName!.value = '' //reset input
 	}
+
+	defaultFilter!.checked = true
+	changeTotalTodos()
 }
 
 /* CHANGE for TODO */
@@ -87,12 +101,31 @@ function handleChangeTodo({ target }: any) {
 	if (target.classList[0] == 'item__status') {
 		for (let todo of target.parentElement.children) {
 			if (todo.className == 'item__status') {
+				todo.checked == !todo.checked
 				target.parentElement.classList.toggle('checked')
 				todos = todos.map((item) => (item.id == target.parentElement.id ? { ...item, status: todo.checked } : item)) // add change status for selected TODO
 			}
 		}
 		setLocalTodos() // set to localStorage
 	}
+
+	changeTotalTodos()
+}
+
+function handlefilteredTodos({ target }: any) {
+	target.value == 'all' ? (filteredTodos = todos) : ''
+	target.value == 'active' ? (filteredTodos = todos.filter((todo) => todo.status == false)) : ''
+	target.value == 'completed' ? (filteredTodos = todos.filter((todo) => todo.status == true)) : ''
+	getTodos()
+}
+
+function handleRemoveCompletedTodos() {
+	defaultFilter!.checked = true
+	todos = todos.filter((todo) => todo.status == false)
+	console.log(todos)
+	filteredTodos = todos
+	setLocalTodos()
+	getTodos()
 }
 
 function onMounted() {
@@ -108,15 +141,19 @@ function onMounted() {
 		todos = []
 	} else {
 		todos = getLocalTodos()
+		filteredTodos = todos
 		getTodos()
 	}
+
+	changeTotalTodos()
 }
 
 function getTodos() {
+	todoList!.innerHTML = ''
 	/* Create TODOS if localstorage is not empty */
-	for (let todo of todos) {
+	for (let todo of filteredTodos) {
 		todoList!.innerHTML += `
-		<li id="${todo.id}" class="todos-list__item">
+		<li id="${todo.id}" class="todos-list__item ${todo.status ? 'checked' : ''}">
 			<input type="checkbox" class="item__status" ${todo.status ? 'checked' : ''}>
    			<input type="text" value="${todo.title}" readonly class="item__title"/>
 			<span>${todo.category}</span>
